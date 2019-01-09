536b74f6f9e3298a5d2954add92977d4b0a76f5b
alessandromenini <alessandro.menini3@gmail.com>
2019-01-09 11:47:31 +0100

    fix add favicon

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,13 +2,13 @@ module.exports = {
   title: 'Amber',
   description: 'Bitrock Design System',
   head: [
-    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }]
-    ['link', { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' }]
-    ['link', { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' }]
-    ['link', { rel: 'manifest', href: '/site.webmanifest' }]
-    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#eb6400' }]
-    ['meta', { name: 'msapplication-TileColor', content: '#da532c' }]
-    ['meta', { name: 'theme-color', content: '#ffffff' }]
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#eb6400' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
   ],
   dest: './dist',
   themeConfig: {
