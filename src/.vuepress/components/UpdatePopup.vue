3e5bdbdead17ecabfc58707a20c0db698338e193
Anthony Penna <penna1296@gmail.com>
2019-02-11 10:57:32 +0100

    replaced default update popup with amber banner

---
+++ src/.vuepress/components/UpdatePopup.vue
@@ -0,0 +1,18 @@
+<template>
+  <SWUpdatePopup>
+    <amber-banner
+      state="defult"
+      title="New content available."
+      labels="Refresh"
+      active
+    />
+  </SWUpdatePopup>
+</template>
+
+<script>
+import SWUpdatePopup from "@vuepress/plugin-pwa/lib/SWUpdatePopup.vue";
+
+export default {
+  components: { SWUpdatePopup }
+};
+</script>
